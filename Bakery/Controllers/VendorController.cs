bb4e6263867b387ea88435779b7ce2380aefe349
James Henager <jameschenager@gmail.com>
2020-07-24 16:00:00 -0700

    add vendor index and vendor new, as well as vendor controller routes to both

--- Bakery/Controllers/VendorController.cs
+++ Bakery/Controllers/VendorController.cs
@@ -1,1 +1,22 @@
-// all logic, routes, and views meant to work with 
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+
+namespace Bakery.Controllers
+{
+  public class VendorController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+  }
+}
