81fe09049664b42c753910a443d37afc21680e33
James Henager <jameschenager@gmail.com>
2020-07-24 13:23:47 -0700

    add passing test for creating instance of order object

---
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bakery.Models;
+using System;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class OrderTest
+  {
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
