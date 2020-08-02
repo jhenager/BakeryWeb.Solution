3fd367d53d6c398b290fec26df21ad751303fb6d
James Henager <jameschenager@gmail.com>
2020-08-01 19:41:32 -0700

    add display details for new order parameters

--- Bakery/Controllers/VendorController.cs
+++ Bakery/Controllers/VendorController.cs
@@ -40,11 +40,11 @@ namespace Bakery.Controllers
   
     
     [HttpPost("/vendors/{vendorsId}/orders")]
-    public ActionResult Create(int vendorId, string orderName)
+    public ActionResult Create(int vendorId, string orderName, string description, int price, string date)
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
       Vendor foundVendor = Vendor.Find(vendorId);
-      Order newOrder = new Order(orderName, "d", 5, "a");
+      Order newOrder = new Order(orderName, description, price, date);
       foundVendor.AddOrder(newOrder);
       List<Order> vendorOrders = foundVendor.Orders;
       model.Add("orders", vendorOrders);
